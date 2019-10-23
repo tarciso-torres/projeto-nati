42637f23b046a91f2f33873138401f6dd13ac76d
Tarciso Torres <tarcisoftorres@gmail.com>
2019-10-23 15:01:59 -0300

    Create migration to aluno

--- src/main/resources/db/migration/V01__create_table_aluno.sql
+++ src/main/resources/db/migration/V01__create_table_aluno.sql
@@ -1,7 +1,28 @@
 CREATE TABLE tb_aluno(
-    id          UUID NOT NULL,
-    nome    	VARCHAR(255),
-    matricula   VARCHAR NOT NULL,
-	email       VARCHAR NOT NULL,
-	PRIMARY KEY (id)
+    id                      UUID NOT NULL,
+    nome                	VARCHAR(255),
+    matricula               VARCHAR(10) NOT NULL,
+	email                   VARCHAR(30) NOT NULL,
+    cpf                     VARCHAR(11) NOT NULL,
+    anoAdmissao             VARCHAR(4) NOT NULL,
+    semestreAdmissao        CHAR(1) NOT NULL,
+    sexo                    CHAR(1) NOT NULL,
+    status                  VARCHAR(11),
+    celNumero               VARCHAR(11),
+    resNumero               VARCHAR(11),
+    comNumero               VARCHAR(11),
+    logradouro              VARCHAR(30),
+    numero                  VARCHAR(5),
+    complemento             VARCHAR(30),
+    bairro                  VARCHAR(20),
+    CEP                     CHAR(8),
+    cidade                  VARCHAR(25),
+    estado                  CHAR(2)
+
 );
+
+ALTER TABLE tb_aluno add constraint pk_aluno_id PRIMARY KEY(id);
+
+alter table tb_aluno ADD constraint ck_aluno check(sexo in('M','F'));
+
+ALTER TABLE tb_aluno ADD CONSTRAINT uk_aluno_email UNIQUE (email);
