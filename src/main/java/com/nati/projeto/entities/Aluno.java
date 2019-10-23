35f2b5551cae17f3b34606d0d6a21dccd8a04f37
Tarciso Torres <tarcisoftorres@gmail.com>
2019-10-23 15:42:30 -0300

    Add bean validations

--- src/main/java/com/nati/projeto/entities/Aluno.java
+++ src/main/java/com/nati/projeto/entities/Aluno.java
@@ -7,16 +7,21 @@ import javax.persistence.Table;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import org.hibernate.validator.constraints.br.CPF;
 
 import com.nati.projeto.entities.enums.AlunoStatus;
 
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "tb_aluno")
-@Data()
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
 public class Aluno extends EntidadeComUUID{
 	
 	@NotBlank(message = "aluno-1")
@@ -26,25 +31,24 @@ public class Aluno extends EntidadeComUUID{
 	@Email(message = "aluno-2") @NotBlank(message = "aluno-3")
 	private String email;
 	
-	@CPF(message = "aluno-4") @NotBlank(message = "aluno-5")
+	@CPF(message = "aluno-4") @NotBlank(message = "aluno-5") @Size(max = 11, min = 11, message = "aluno-6")
 	private String cpf;
 	
-	@NotBlank(message = "aluno-6")
+	@NotBlank(message = "aluno-7")
 	@Column(name = "anoadmissao")
 	private String anoAdmissao;
 	
-	@NotBlank(message = "aluno-7")
+	@NotBlank(message = "aluno-8")
 	@Column(name = "semestreadmissao")
 	private String semestreAdmissao;
 	
-	@NotBlank(message = "aluno-8")
+	@NotBlank(message = "aluno-9")
 	private String sexo;
 	
-	@NotNull(message =  "aluno-9")
+	@NotNull(message =  "aluno-10")
 	private AlunoStatus status;
 	
 	@Embedded
-	
 	private Telefone telefone;
 	
 	@Embedded
