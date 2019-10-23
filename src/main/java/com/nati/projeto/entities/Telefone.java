a1e697fafa5dbc626a4523f3c28a337edba3afd9
Tarciso Torres <tarcisoftorres@gmail.com>
2019-10-23 17:25:16 -0300

    Add some beans validations

--- src/main/java/com/nati/projeto/entities/Telefone.java
+++ src/main/java/com/nati/projeto/entities/Telefone.java
@@ -2,7 +2,8 @@ package com.nati.projeto.entities;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -12,13 +13,15 @@ import lombok.Setter;
 @Setter
 public class Telefone {
 
-	@NotNull(message = "telefone-1")
+	@NotBlank(message = "telefone-1") @Size(min = 11, max = 11, message = "telefone-2")
 	@Column(name = "celnumero")
 	private String celNumero;
 
+	@Size(min = 11, max = 11, message = "telefone-3")
 	@Column(name = "resnumero")
 	private String resNumero;
 
+	@Size(min = 11, max = 11, message = "telefone-4")
 	@Column(name = "comnumero")
 	private String comNumero;
 
