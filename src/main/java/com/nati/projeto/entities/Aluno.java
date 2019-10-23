ebce9d70e2631500d8625dbc8512271e08f8c4ff
Tarciso Torres <tarcisoftorres@gmail.com>
2019-10-23 15:02:36 -0300

    Create model to aluno

--- src/main/java/com/nati/projeto/entities/Aluno.java
+++ src/main/java/com/nati/projeto/entities/Aluno.java
@@ -1,17 +1,53 @@
 package com.nati.projeto.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.br.CPF;
+
+import com.nati.projeto.entities.enums.AlunoStatus;
 
 import lombok.Data;
 
 @Entity
 @Table(name = "tb_aluno")
-@Data
+@Data()
 public class Aluno extends EntidadeComUUID{
 	
+	@NotBlank(message = "aluno-1")
 	private String nome;
 	private String matricula;
+	
+	@Email(message = "aluno-2") @NotBlank(message = "aluno-3")
 	private String email;
+	
+	@CPF(message = "aluno-4") @NotBlank(message = "aluno-5")
+	private String cpf;
+	
+	@NotBlank(message = "aluno-6")
+	@Column(name = "anoadmissao")
+	private String anoAdmissao;
+	
+	@NotBlank(message = "aluno-7")
+	@Column(name = "semestreadmissao")
+	private String semestreAdmissao;
+	
+	@NotBlank(message = "aluno-8")
+	private String sexo;
+	
+	@NotNull(message =  "aluno-9")
+	private AlunoStatus status;
+	
+	@Embedded
+	
+	private Telefone telefone;
+	
+	@Embedded
+	private Endereco endereco;
 
 }
