3056c9d5b80080b7054a2caa0b270842b7a56d72
Tarciso Torres <tarcisoftorres@gmail.com>
2019-10-24 16:06:48 -0300

    Add validation to cep in class Endereco

--- src/main/java/com/nati/projeto/entities/Endereco.java
+++ src/main/java/com/nati/projeto/entities/Endereco.java
@@ -1,6 +1,7 @@
 package com.nati.projeto.entities;
 
 import javax.persistence.Embeddable;
+import javax.validation.constraints.Size;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -13,8 +14,12 @@ public class Endereco {
 	private String numero;
 	private String complemento;
 	private String bairro;
+	
+	@Size(min = 8, max = 8, message = "endereco-1")
 	private String cep;
 	private String cidade;
+	
+	@Size(min = 2, max = 2, message = "endereco-2")
 	private String estado;
 
 }
