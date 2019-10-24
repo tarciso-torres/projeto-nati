2947cb3a54ec712a67f5cfb45c9c2ae408d6ae9d
Tarciso Torres <tarcisoftorres@gmail.com>
2019-10-24 15:31:06 -0300

    Create repository to coordenador

---
+++ src/main/java/com/nati/projeto/repositories/CoordenadorRepository.java
@@ -0,0 +1,19 @@
+package com.nati.projeto.repositories;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.nati.projeto.entities.Coordenador;
+
+@Repository
+public interface CoordenadorRepository extends JpaRepository<Coordenador, UUID>{
+	
+	Coordenador findByDadosPessoaisMatricula(String matricula);
+	
+	Coordenador findByDadosPessoaisCpf(String cpf);
+	
+	Coordenador findByDadosPessoaisEmail(String email);
+
+}
