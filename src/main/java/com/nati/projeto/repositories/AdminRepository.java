a35df3487cd4d14f886ce3a68667793bd5215282
Tarciso Torres <tarcisoftorres@gmail.com>
2019-10-25 15:01:47 -0300

    Create repository to Admin
    Add findByNome, findByCpf, findByMatricula

---
+++ src/main/java/com/nati/projeto/repositories/AdminRepository.java
@@ -0,0 +1,19 @@
+package com.nati.projeto.repositories;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.nati.projeto.entities.Admin;
+
+@Repository
+public interface AdminRepository extends JpaRepository<Admin, UUID>{
+	
+	Admin findByDadosPessoaisMatricula(String name);
+	
+	Admin findByDadosPessoaisCpf(String cpf);
+	
+	Admin findByDadosPessoaisEmail(String email);
+
+}
